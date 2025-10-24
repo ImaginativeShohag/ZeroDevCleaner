c61028b507d80ea57a82a978d79ab469de55f0a7
Md. Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2025-10-25 03:01:45 +0600

    Initial Commit

---
+++ ZeroDevCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZeroDevCleaner
+//
+//  Created by Md. Mahmudul Hasan Shohag on 25/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZeroDevCleaner/ZeroDevCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroDevCleanerApp.swift
+//  ZeroDevCleaner
+//
+//  Created by Md. Mahmudul Hasan Shohag on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ZeroDevCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
